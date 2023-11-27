cce46d887dcbae769b9b5078c19847a1d97df197
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2023-11-27 22:53:21 +0000

    Home screen init

---
+++ screens/homeSceen.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default  function HomeSceen () {
+  return (
+    <View>
+      <Text>homeSceen</Text>
+    </View>
+  )
+}
+
+
+
+const styles = StyleSheet.create({})
