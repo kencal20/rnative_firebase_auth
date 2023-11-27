062616dee26a5a78dc9ac7c5e2e05ab82451a32a
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2023-11-27 22:54:20 +0000

    addition of firebase config file

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC6YtgLj8jGyM_V5WJl6xqyJgfIF33IVcU",
+  authDomain: "fir-exponative.firebaseapp.com",
+  projectId: "fir-exponative",
+  storageBucket: "fir-exponative.appspot.com",
+  messagingSenderId: "1071408420831",
+  appId: "1:1071408420831:web:6fc2110986ff6c66a5566a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
