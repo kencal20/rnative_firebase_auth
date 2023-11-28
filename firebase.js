de6442431ce1a7daed8e9af4a2d82273a1715df4
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2023-11-28 11:35:46 +0000

    adding firebase auth setup

--- firebase.js
+++ firebase.js
@@ -1,17 +1,18 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+import { initializeApp } from 'firebase/app'
+import { getAuth } from "firebase/auth";
+import { getFirestore } from 'firebase/firestore'
+
 const firebaseConfig = {
-  apiKey: "AIzaSyC6YtgLj8jGyM_V5WJl6xqyJgfIF33IVcU",
-  authDomain: "fir-exponative.firebaseapp.com",
-  projectId: "fir-exponative",
-  storageBucket: "fir-exponative.appspot.com",
-  messagingSenderId: "1071408420831",
-  appId: "1:1071408420831:web:6fc2110986ff6c66a5566a"
+    apiKey: "AIzaSyC6YtgLj8jGyM_V5WJl6xqyJgfIF33IVcU",
+    authDomain: "fir-exponative.firebaseapp.com",
+    projectId: "fir-exponative",
+    storageBucket: "fir-exponative.appspot.com",
+    messagingSenderId: "1071408420831",
+    appId: "1:1071408420831:web:6fc2110986ff6c66a5566a"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// // Initialize Firebase
+export const FIREBASE_APP = initializeApp(firebaseConfig)
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP)
+export const FIREBASE_DB = getFirestore(FIREBASE_APP)
