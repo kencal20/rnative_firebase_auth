b2f51de7413c52eb476329bb7823206a06b114f1
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2023-11-28 11:38:14 +0000

    adding firebase authentication for signin and signup and adding activity indicator

--- screens/loginScreen.jsx
+++ screens/loginScreen.jsx
@@ -1,17 +1,52 @@
+import React, { useState } from "react";
 import {
   View,
   Text,
   KeyboardAvoidingView,
   TextInput,
   TouchableOpacity,
+  StyleSheet,
+  ActivityIndicator,
+  Button,
 } from "react-native";
-import React, { useState } from "react";
-import { StyleSheet } from "react-native";
+import { FIREBASE_AUTH} from "../firebase";
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+
+
 
 export default function LoginScreen() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
+
+  const auth = FIREBASE_AUTH;
+  const handleLogin = async () => {
+    setLoading(true);
+    try {
+      const response = await signInWithEmailAndPassword(auth,email, password);
+      console.log(response);
+      alert('account registered')
+    } catch (error) {
+        console.log(error);
+        alert('SignIN failed:' + error.message)
+    }finally{
+        setLoading(false)
+    }
+  };
+handleSignup= async()=>{
+    setLoading(true);
+    try {
+      const response = await createUserWithEmailAndPassword(auth,email, password);
+      console.log(response);
+      alert('check your email')
+    } catch (error) {
+        console.log(error);
+        alert('SignIN failed:' + error.message)
+    }finally{
+        setLoading(false)
+    }
+}
   return (
     <KeyboardAvoidingView style={styles.container}>
       <View style={styles.inputContainer}>
@@ -20,28 +55,39 @@ export default function LoginScreen() {
           style={styles.input}
           value={email}
           onChangeText={(text) => setEmail(text)}
+          autoCapitalize="none"
         />
         <TextInput
-          placeholder="password"
+          placeholder="Password"
           style={styles.input}
           value={password}
           onChangeText={(text) => setPassword(text)}
+          autoCapitalize="none"
           secureTextEntry
         />
       </View>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Login</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={[styles.button, styles.buttonOutline]}>
-          <Text style={[styles.buttonText, styles.buttonOutlineText]}>
-            Register
-          </Text>
-        </TouchableOpacity>
-      </View>
+
+      {loading ? (
+        <ActivityIndicator size="large" color="#000ff" />
+      ) : (
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={handleLogin}>
+            <Text style={styles.buttonText}>Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.button, styles.buttonOutline]}
+            onPress={handleSignup}
+          >
+            <Text style={[styles.buttonText, styles.buttonOutlineText]}>
+              Register
+            </Text>
+          </TouchableOpacity>
+        </View>
+      )}
     </KeyboardAvoidingView>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
